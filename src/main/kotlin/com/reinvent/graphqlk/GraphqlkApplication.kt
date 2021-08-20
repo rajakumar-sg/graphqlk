1445beea2e644907db06e6d4be371a777f2dea70
raj <torajakumar@gmail.com>
2021-08-20 21:13:55 +0800

    init

---
+++ src/main/kotlin/com/reinvent/graphqlk/GraphqlkApplication.kt
@@ -0,0 +1,11 @@
+package com.reinvent.graphqlk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlkApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlkApplication>(*args)
+}
---
+++ src/test/kotlin/com/reinvent/graphqlk/GraphqlkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reinvent.graphqlk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
