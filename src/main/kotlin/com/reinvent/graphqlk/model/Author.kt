875d569438623035ccaf13e56c94e38aaeb56212
raj <torajakumar@gmail.com>
2021-08-22 19:30:18 +0800

    property with enum argument

--- src/main/kotlin/com/reinvent/graphqlk/model/Author.kt
+++ src/main/kotlin/com/reinvent/graphqlk/model/Author.kt
@@ -9,4 +9,12 @@ data class Author(
     companion object {
         val none = Author(ID(""), "")
     }
+
+    fun caseName(caseType: CaseType) = caseType.apply(name)
+}
+
+enum class CaseType(private val converter : (String) -> String) {
+    UPPER(String::uppercase), LOWER(String::lowercase);
+
+    fun apply(name: String) = converter(name)
 }
